a94199c931a8b851702257c8d36826f7a9ede543
h.w zisee zhou <fzzhgj@gmail.com>
2013-11-10 15:15:36 +0800

    Try td contextmenu forward to input

--- swsc/body_weekly_report.php
+++ swsc/body_weekly_report.php
@@ -16,23 +16,91 @@ PG_ASSERT(_local_file_load('common'));
 <script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
 <script type="text/javascript" src="js/jquery.tipsy.js"></script>
 <script type="text/javascript" src="js/plbtn.js"></script>
+<script type="text/javascript" src="js/jquery.autosize.min.js"></script>
 <script type="text/javascript">
 $(document).ready(function(){
-    $("#w_add").plbtn({click:function(){
+    $('#w_add').plbtn({click:function(){
         $('#report_review').css('display', 'none');
         $('#report_new').css('display', 'block');
     }});
-    $("#w_this").plbtn({click:function(){
+    $('#w_this').plbtn({click:function(){
         $('#report_new').css('display', 'none');
         $('#report_review').css('display', 'block');
     }});
-    $("#w_last").plbtn({click:function(){alert('w_last');}});
-    $("#w_query").plbtn({click:function(){alert('w_query');}});
+    $('#w_last').plbtn({click:function(){alert('w_last');}});
+    $('#w_query').plbtn({click:function(){alert('w_query');}});
 
-    $("#w_add").plbtn('addIcon', 'img/icon/add.png');
-    $("#w_query").tipsy({delayIn:500, fallback:"<?=_('btn_w_date_eg');?>"});
+    $('#w_add').plbtn('addIcon', 'img/icon/add.png');
+    $('#w_query').tipsy({delayIn:500, fallback:"<?=_('btn_w_date_eg');?>"});
 
-    $("#w_this").click();
+    $('#w_this').click();
+
+    $('#w_add_newline').plbtn({click:function(){alert('w_add_newline');}});
+    $('#w_add_save_all').plbtn({click:function(){alert('w_add_save_all');}});
+    $('#w_add_newline').plbtn('addIcon', 'img/icon/add.png');
+    $('#w_add_save_all').plbtn('addIcon', 'img/icon/save.png');
+
+
+    $('textarea').autosize();
+
+    $('.elem_input td').each(function(){
+        var childInput = $(this).children('input');
+        var childTextArea = $(this).children('textarea');
+        if (childInput.length || childTextArea.length) {
+            $(this).click(function(){
+                if (childInput.length) {
+                    childInput.focus();
+                } else {
+                    childTextArea.focus();
+                }
+            });
+        }
+
+        if (childTextArea.length) {
+            childTextArea.css('width', $(this).css('width'));
+            /*
+            $(this).mousedown(function(e) {
+                //e.stopPropagation(); // avoid triggering its parent
+                //alert(e.target.type);
+                //return false;
+                e.stopPropagation(); // avoid triggering its parent
+                e.preventDefault();
+                return false;
+            });
+            
+            $(this).mouseup(function(e) {
+                e.stopPropagation(); // avoid triggering its parent
+                e.preventDefault();
+                return false;
+                //alert(e.target.type);
+                //return false;
+            });
+            //($this).mousedown(function(e) {
+            //    //childTextArea.trigger('mouseup');
+            //    alert(e.currentTarget+', '+event.relatedTarget);
+            //});*/
+            
+            $(this).bind("contextmenu",function(e){
+                if (e.target.type=='textarea') {
+                    return true;
+                } else {
+                    childTextArea.focus();
+                    //childTextArea.trigger('mousedown', {type:'mousedown', button:2});
+                    //childTextArea.trigger('mouseup', {type:'mouseup', button:2});
+                    //childTextArea.trigger('contextmenu', e);
+                    //childTextArea.trigger({
+                    //    type: 'mousedown',
+                    //    which: 3
+                    //});
+                    //e.target.type=='textarea'
+                    //childTextArea.trigger('contextmenu');
+                    return false;
+                }
+            });
+        }
+    });
+
+
 });
 </script>
 <style type="text/css">
@@ -48,7 +116,7 @@ $(document).ready(function(){
     margin-left: -3px;
 }
 
-input[type=text], input[type=password] {
+.body_toolbar input[type=text], input[type=password] {
     transition: all 0.20s ease-in-out;
     -webkit-transition: all 0.20s ease-in-out;
     -moz-transition: all 0.20s ease-in-out;
@@ -56,7 +124,7 @@ input[type=text], input[type=password] {
     border-radius: 3px;
     outline: none;
 }
-input[type=text]:focus,input[type=password]:focus,textarea:focus{
+.body_toolbar input[type=text]:focus,input[type=password]:focus{
     border:1px solid black;
     box-shadow:0 0 5px rgba(103,166,217,1);
     -moz-box-shadow:0 0 5px rgba(103,166,217,1);
@@ -69,6 +137,31 @@ input[type=text]:focus,input[type=password]:focus,textarea:focus{
 .tableHeader, .tableDate {
     font-weight: bold;
     border: solid 2px black;
+}
+#report_new input, textarea {
+    width: 100%;
+    font-size: 1.2em;
+    color: #0000CD;
+    border: 0;
+}
+#report_new input[type=text]:focus, textarea:focus {
+    border: 0;
+    outline: none;
+}
+#report_new input {
+    height: 1.2em;
+}
+#report_new_toolbar {
+    margin-top: 15px;
+}
+.elem_input {
+    color: #0000CD;
+}
+.elem_input td {
+    vertical-align: top;
+}
+.elem_input textarea {
+    resize: none;
 }
 </style>
 </head>
@@ -90,25 +183,22 @@ input[type=text]:focus,input[type=password]:focus,textarea:focus{
     <div class="workspace">
         <div id="report_review">
         <table border="0" cellpadding="1" cellspacing="0">
-          <col width="65" />
+          <col width="64" />
           <col width="64" span="4" />
-          <col width="241" />
+          <col width="240" />
           <col width="64" span="2" />
           <tr class="tableDate">
-            <td colspan="5" width="321">时间：2013年8月12日-2013年8月16日</td>
-            <td width="241"></td>
-            <td width="64"></td>
-            <td width="64"></td>
+            <td colspan="8" width="688">时间：2013年8月12日-2013年8月16日</td>
           </tr>
           <tr class="tableHeader">
-            <td>姓名</td>
-            <td width="64">项目名称</td>
-            <td width="64">项目类型</td>
-            <td width="64">项目阶段</td>
-            <td width="64">工作地点</td>
-            <td width="241">工作内容</td>
-            <td width="64">加班时长</td>
-            <td width="64">行程时长</td>
+            <td><?=_('table_header_Name');?></td>
+            <td width="64"><?=_('table_header_proName');?></td>
+            <td width="64"><?=_('table_header_proType');?></td>
+            <td width="64"><?=_('table_header_proStage');?></td>
+            <td width="64"><?=_('table_header_workAddress');?></td>
+            <td width="240"><?=_('table_header_workContent');?></td>
+            <td width="64"><?=_('table_header_extraWorktime');?></td>
+            <td width="64"><?=_('table_header_transDuration');?></td>
           </tr>
           <tr>
             <td>苏磊</td>
@@ -116,7 +206,7 @@ input[type=text]:focus,input[type=password]:focus,textarea:focus{
             <td width="64">非公开</td>
             <td width="64">答复反馈意见</td>
             <td width="64">青岛</td>
-            <td width="241">补充半年报及答复反馈意见</td>
+            <td width="240">补充半年报及答复反馈意见</td>
             <td width="64">0</td>
             <td width="64">　</td>
           </tr>
@@ -126,7 +216,7 @@ input[type=text]:focus,input[type=password]:focus,textarea:focus{
             <td width="64">IPO</td>
             <td width="64">补半年报</td>
             <td width="64">北京</td>
-            <td width="241">更新财务数据</td>
+            <td width="240">更新财务数据</td>
             <td width="64">　</td>
             <td width="64">　</td>
           </tr>
@@ -136,7 +226,7 @@ input[type=text]:focus,input[type=password]:focus,textarea:focus{
             <td width="64">IPO</td>
             <td width="64">保荐阶段</td>
             <td width="64">北京</td>
-            <td width="241">更新半年报及反馈意见</td>
+            <td width="240">更新半年报及反馈意见</td>
             <td width="64">--</td>
             <td width="64">--</td>
           </tr>
@@ -146,7 +236,7 @@ input[type=text]:focus,input[type=password]:focus,textarea:focus{
             <td width="64">资产重组</td>
             <td width="64">尽职调查</td>
             <td width="64">北京</td>
-            <td width="241">尽职调查</td>
+            <td width="240">尽职调查</td>
             <td width="64">无</td>
             <td width="64">无</td>
           </tr>
@@ -156,7 +246,7 @@ input[type=text]:focus,input[type=password]:focus,textarea:focus{
             <td width="64">非公开</td>
             <td width="64">辅导&amp;尽职调查</td>
             <td width="64">北京</td>
-            <td width="241">长征电气过往三年及一期关联交易情况核查、上次募投项目、本次募投项目内容及重大合同核对</td>
+            <td width="240">长征电气过往三年及一期关联交易情况核查、上次募投项目、本次募投项目内容及重大合同核对</td>
             <td width="64">0小时</td>
             <td width="64">0小时</td>
           </tr>
@@ -166,7 +256,7 @@ input[type=text]:focus,input[type=password]:focus,textarea:focus{
             <td width="64">资产重组</td>
             <td width="64">封卷</td>
             <td width="64">北京</td>
-            <td width="241">制作同方要求的申报材料</td>
+            <td width="240">制作同方要求的申报材料</td>
             <td width="64">0小时</td>
             <td width="64">0小时</td>
           </tr>
@@ -175,7 +265,7 @@ input[type=text]:focus,input[type=password]:focus,textarea:focus{
             <td width="64">IPO</td>
             <td width="64">审核反馈</td>
             <td width="64">浙江宁波、辽宁阜新</td>
-            <td width="241">走访德基机械客户</td>
+            <td width="240">走访德基机械客户</td>
             <td width="64">0小时</td>
             <td width="64">10小时</td>
           </tr>
@@ -185,7 +275,7 @@ input[type=text]:focus,input[type=password]:focus,textarea:focus{
             <td width="64">非公开</td>
             <td width="64">反馈答复</td>
             <td width="64">非现场</td>
-            <td width="241">针对问题进行进一步讨论</td>
+            <td width="240">针对问题进行进一步讨论</td>
             <td width="64">　</td>
             <td width="64">　</td>
           </tr>
@@ -194,7 +284,7 @@ input[type=text]:focus,input[type=password]:focus,textarea:focus{
             <td width="64">IPO</td>
             <td width="64">补充中期材料</td>
             <td width="64">非现场</td>
-            <td width="241">补充反馈恢复中7月回款情况</td>
+            <td width="240">补充反馈恢复中7月回款情况</td>
             <td width="64">　</td>
             <td width="64">　</td>
           </tr>
@@ -204,7 +294,7 @@ input[type=text]:focus,input[type=password]:focus,textarea:focus{
             <td width="64">并购</td>
             <td width="64">尽职调查</td>
             <td width="64">高阳捷迅现场</td>
-            <td width="241">现场尽职调查</td>
+            <td width="240">现场尽职调查</td>
             <td width="64">　</td>
             <td width="64">　</td>
           </tr>
@@ -214,7 +304,7 @@ input[type=text]:focus,input[type=password]:focus,textarea:focus{
             <td width="64">IPO</td>
             <td width="64">辅导</td>
             <td width="64">北京</td>
-            <td width="241">撰写业务与技术章节，跟踪蓝天环科模拟合并问题</td>
+            <td width="240">撰写业务与技术章节，跟踪蓝天环科模拟合并问题</td>
             <td width="64">　</td>
             <td width="64">　</td>
           </tr>
@@ -223,7 +313,7 @@ input[type=text]:focus,input[type=password]:focus,textarea:focus{
             <td width="64">IPO</td>
             <td width="64">改制</td>
             <td width="64">北京</td>
-            <td width="241">召开中介会，准备股改相关工作，督促企业尽快股改</td>
+            <td width="240">召开中介会，准备股改相关工作，督促企业尽快股改</td>
             <td width="64">　</td>
             <td width="64">　</td>
           </tr>
@@ -232,7 +322,7 @@ input[type=text]:focus,input[type=password]:focus,textarea:focus{
             <td width="64">非公开</td>
             <td width="64">尽调</td>
             <td width="64">北京</td>
-            <td width="241">尽职调查，撰写尽调报告</td>
+            <td width="240">尽职调查，撰写尽调报告</td>
             <td width="64">　</td>
             <td width="64">　</td>
           </tr>
@@ -241,7 +331,7 @@ input[type=text]:focus,input[type=password]:focus,textarea:focus{
             <td width="64">重组</td>
             <td width="64">发行</td>
             <td width="64">北京</td>
-            <td width="241">准备发行阶段相关文件</td>
+            <td width="240">准备发行阶段相关文件</td>
             <td width="64">　</td>
             <td width="64">　</td>
           </tr>
@@ -251,7 +341,7 @@ input[type=text]:focus,input[type=password]:focus,textarea:focus{
             <td width="64">IPO</td>
             <td width="64">补充中报</td>
             <td width="64">北京</td>
-            <td width="241">访谈客户、补充招股书</td>
+            <td width="240">访谈客户、补充招股书</td>
             <td width="64">6</td>
             <td width="64">5</td>
           </tr>
@@ -260,7 +350,7 @@ input[type=text]:focus,input[type=password]:focus,textarea:focus{
             <td width="64">非公开</td>
             <td width="64">上报材料</td>
             <td width="64">北京</td>
-            <td width="241">答复反馈意见、与预审员沟通</td>
+            <td width="240">答复反馈意见、与预审员沟通</td>
             <td width="64">　</td>
             <td width="64">5</td>
           </tr>
@@ -270,7 +360,7 @@ input[type=text]:focus,input[type=password]:focus,textarea:focus{
             <td width="64">并购</td>
             <td width="64">尽职调查</td>
             <td width="64">北京</td>
-            <td width="241">写交易报告书</td>
+            <td width="240">写交易报告书</td>
             <td width="64">　</td>
             <td width="64">　</td>
           </tr>
@@ -278,46 +368,72 @@ input[type=text]:focus,input[type=password]:focus,textarea:focus{
         </div>
         <div id="report_new">
         <table border="0" cellpadding="1" cellspacing="0">
-          <col width="65" />
+          <col width="64" />
           <col width="64" span="4" />
-          <col width="241" />
-          <col width="64" span="2" />
+          <col width="240" />
+          <col width="64" span="3" />
           <tr class="tableDate">
-            <td colspan="5" width="321">时间：2013年8月12日-2013年8月16日</td>
-            <td width="241"></td>
-            <td width="64"></td>
-            <td width="64"></td>
+            <td colspan="9" width="752">时间：2013年8月12日-2013年8月16日</td>
           </tr>
           <tr class="tableHeader">
-            <td>姓名</td>
-            <td width="64">项目名称</td>
-            <td width="64">项目类型</td>
-            <td width="64">项目阶段</td>
-            <td width="64">工作地点</td>
-            <td width="241">工作内容</td>
-            <td width="64">加班时长</td>
-            <td width="64">行程时长</td>
+            <td><?=_('table_header_Name');?></td>
+            <td width="64"><?=_('table_header_proName');?></td>
+            <td width="64"><?=_('table_header_proType');?></td>
+            <td width="64"><?=_('table_header_proStage');?></td>
+            <td width="64"><?=_('table_header_workAddress');?></td>
+            <td width="240"><?=_('table_header_workContent');?></td>
+            <td width="64"><?=_('table_header_extraWorktime');?></td>
+            <td width="64"><?=_('table_header_transDuration');?></td>
+            <td width="64"><?=_('table_header_actions');?></td>
           </tr>
           <tr>
-            <td rowspan="2">侯泱</td>
+            <td rowspan="3"><? echo $_SESSION['real_name']; ?></td>
             <td width="64">同方股份</td>
             <td width="64">资产重组</td>
             <td width="64">封卷</td>
             <td width="64">北京</td>
-            <td width="241">制作同方要求的申报材料</td>
-            <td width="64">0小时</td>
-            <td width="64">0小时</td>
+            <td width="240">制作同方要求的申报材料</td>
+            <td width="64">0</td>
+            <td width="64">0</td>
+            <td></td>
           </tr>
           <tr>
             <td width="64">德基机械</td>
             <td width="64">IPO</td>
             <td width="64">审核反馈</td>
             <td width="64">浙江宁波、辽宁阜新</td>
-            <td width="241">走访德基机械客户</td>
+            <td width="240">走访德基机械客户</td>
+            <td width="64">0</td>
+            <td width="64">10</td>
+            <td></td>
+          </tr>
+
+          <tr class="elem_input">
+            <td width="64"><textarea rows="1"></textarea></td>
+            <td width="64"><textarea rows="1"></textarea></td>
+            <td width="64"><textarea rows="1"></textarea></td>
+            <td width="64"><textarea rows="1"></textarea></td>
+            <td width="240"><textarea rows="1"></textarea></td>
+            <td width="64"><input type="text" name="a" maxlength="3" /></td>
+            <td width="64"><input type="text" name="a" maxlength="3" /></td>
+            <td></td>
+          </tr>
+          <!--
+          <tr>
+            <td width="64">德基机械</td>
+            <td width="64">IPO</td>
+            <td width="64">审核反馈</td>
+            <td width="64">浙江宁波、辽宁阜新</td>
+            <td width="240">走访德基机械客户</td>
             <td width="64">0小时</td>
             <td width="64">10小时</td>
           </tr>
+          -->
         </table>
+        <div id="report_new_toolbar">
+            <div id="w_add_newline" class="btn_base body_toolbar_item"><?=_('btn_w_add_newline');?></div>
+            <div id="w_add_save_all" class="btn_base body_toolbar_item"><?=_('btn_w_add_save_all');?></div>
+        </div>
         </div>
     </div>
 </body>
